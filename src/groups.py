4e1f8fd3ea26ff0eab8f87750994f57096d73f1a
DB8LE <db8le@darc.de>
2026-04-07 16:05:11 +0200

    Add copy_groups attribute to groups config

--- src/groups.py
+++ src/groups.py
@@ -1,3 +1,4 @@
+import logging
 import tomllib
 from collections import defaultdict
 from typing import Dict, List, Tuple
@@ -6,7 +7,20 @@ def read_groups(file_path: str) -> Tuple[Dict[str, List[int]], Dict[str, List[st
     with open(file_path, "rb") as f:
         data = tomllib.load(f)
 
-    group_ids = {}
+    # Parse copy_groups attribute
+    for name, config in data.items():
+        if "copy_groups" in config.keys():
+            for copy_name in config["copy_groups"]:
+                copy_config = data[copy_name]
+                if "norad_ids" in copy_config.keys():
+                    config["norad_ids"].extend(copy_config["norad_ids"])
+                    config["norad_ids"] = list(set(config["norad_ids"])) # Deduplicate
+                if "inherit_sources" in copy_config.keys():
+                    config["inherit_sources"].extend(copy_config["inherit_sources"])
+                    config["inherit_sources"] = list(set(config["inherit_sources"])) # Deduplicate
+
+    # Parse other attributes
+    group_ids = defaultdict(list)
     source_groups = defaultdict(list)
     for name, config in data.items():
         if "norad_ids" in config.keys():
@@ -16,4 +30,4 @@ def read_groups(file_path: str) -> Tuple[Dict[str, List[int]], Dict[str, List[st
             for source in config["inherit_sources"]:
                 source_groups[source].append(name)
 
-    return group_ids, source_groups
+    return dict(group_ids), dict(source_groups)
